6f4f029c3bbf4af640360a3689099d48b8a88fb4
tuandaodev <tuandao.dev@gmail.com>
2019-05-14 13:48:23 +0700

    Update console-script.js

--- console-script.js
+++ console-script.js
@@ -1,3 +1,3 @@
 Array.prototype.forEach.call(document.querySelectorAll('.r>a:first-child'), function(element) { 
-var str = element.href; var main='banmaynuocnong.com'; str = str.replace('http://www.' + main,''); str = str.replace('https://www.' + main,''); str = str.replace('http://' + main,''); str = str.replace('https://' + main,''); console.log(str);
+var str = element.href; var main='domain.com'; str = str.replace('http://www.' + main,''); str = str.replace('https://www.' + main,''); str = str.replace('http://' + main,''); str = str.replace('https://' + main,''); console.log(str);
 });
